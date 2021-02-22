8fc0d16d458b8d3b3b68dc7ab9e44e250fd82c59
Joe <outofchng@gmail.com>
2021-02-23 00:36:21 +0900

    Add get_library_info to views

--- chatbotapp/views.py
+++ chatbotapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from chatbotapp.cnudata.library import get_library_answer
+
+
+@csrf_exempt
+def get_library_info(request):
+    answer = (request.body.decode('utf-8'))
+    return_json_str = json.load(answer)
+    return_str = return_json_str['userRequest']['utterance']
+
+    if return_str == '열람실':
+        response = get_library_answer()
+        return JsonResponse(response)
