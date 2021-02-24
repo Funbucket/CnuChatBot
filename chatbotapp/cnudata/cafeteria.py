bc0ec7b71ccc5bab7d2503cf1ad1476650081a7a
ChanHyukPark-Tech <pgg6713@gmail.com>
2021-02-24 15:32:02 +0900

    cafeteria answer return 안한것 오류수정

--- chatbotapp/cnudata/cafeteria.py
+++ chatbotapp/cnudata/cafeteria.py
@@ -2,7 +2,5 @@ from chatbotapp.kakaojsonformat.response import *
 from chatbotapp.cnudata.studenthall1_info import *
 
 def get_studenthall1_answer():
-    category()
-
-
-get_studenthall1_answer()
+    answer = category()
+    return answer
