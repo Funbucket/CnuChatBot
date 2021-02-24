ef7f371efd6fff8ec3e542c0ab21e8243890e843
ChanHyukPark-Tech <pgg6713@gmail.com>
2021-02-24 23:49:15 +0900

    해결한듯 browser.quit 지워서

--- chatbotapp/cnudata/studenthall2_info.py
+++ chatbotapp/cnudata/studenthall2_info.py
@@ -19,14 +19,14 @@ class Cafeteria(Enum):
     student_hall_3 = "3"
     sangrok_student_hall = "4"
     college_of_domestic_science = "5"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"}
+# headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"}
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--no-sandbox")
-
-browser = webdriver.Chrome(r"/home/ubuntu/cnuchatbot/chromedriver", options=options)
+url = "http://cnuis.cnu.ac.kr/jsp/etc/weekMenuFrame.jsp"
+browser = webdriver.Chrome(r"C:\Users\woner\Desktop\coding\chatbot_cnu\real_chatbot_project\chatbotapp\cnudata\chromedriver", options=options)
 # 옵션 생성
 # options = webdriver.ChromeOptions()
 # # 대상거부 방지
@@ -39,7 +39,7 @@ browser = webdriver.Chrome(r"/home/ubuntu/cnuchatbot/chromedriver", options=opti
 
 
 def get_soup(frame):
-    url = "http://cnuis.cnu.ac.kr/jsp/etc/weekMenuFrame.jsp"
+
     browser.get(url)
     element = browser.find_element_by_name(frame)
     browser.switch_to.frame(element)
@@ -52,31 +52,31 @@ def get_soup(frame):
 
 # 오늘 날짜와 비교하여서 그날짜 값의 index 반환 > 그 index 를 활용해 오늘 날짜메뉴 얻기
 
-def get_date_index():
-    browser.switch_to.default_content()
-    browser.switch_to.frame("bottom")
-    soup_bottom = BeautifulSoup(browser.page_source, "lxml")
-
-    datas = soup_bottom.find_all("td", attrs={"id": "cc1"})
-    today = ""
-    count = 0
-    for idx, data in enumerate(datas, start=3):
-        data = data.get_text().strip()
-        if (data == now):
-            count = idx
-            break
-    browser.quit()
-    return count
+# def get_date_index():
+#
+#     browser.switch_to.default_content()
+#     browser.switch_to.frame("bottom")
+#     soup_bottom = BeautifulSoup(browser.page_source, "lxml")
+#
+#     datas = soup_bottom.find_all("td", attrs={"id": "cc1"})
+#     today = ""
+#     count = 0
+#     for idx, data in enumerate(datas, start=3):
+#         data = data.get_text().strip()
+#         if (data == now):
+#             count = idx
+#             break
+#     browser.quit()
+#     return count
 
 
 # 조식 , 석식 , 석식 , 일품(석식) 일품(석식) 페이지 띄워주기 !
-def set_timezone(timezone):
-    soup_menu = get_soup("menu")
-    select = Select(browser.find_element_by_name("food_div_cd"))
-    select.select_by_visible_text(timezone)
+# def set_timezone(timezone):
+#     browser.get(url)
+#     element = browser.find_element_by_name("menu")
+#     browser.switch_to.frame(element)
+#     select = Select(browser.find_element_by_name("food_div_cd"))
 
-    # 조회버튼 클릭
-    browser.find_element_by_xpath('/html/body/form/table/tbody/tr[3]/td/table/tbody/tr/td[8]/input').click()
 
 
 # 제 1학생회관을 제외화고 menu 가져옴 조식 석식 석식
@@ -94,11 +94,20 @@ def number_timezone(idx):
 def get_recipe(cafeteria):
     cafeteria_info = {"restaurant": cafeteria.name}
     count = 1  # 조식:1 중식:2 석식:3 일품(중식):4 일품(석식):5
-    # 석식 크롤링
-    answer = ""
-    for when in ["조식", "중식", "석식", "일품(중식)", "일품(석식)"]:
 
-        set_timezone(when)
+    answer = ""
+
+
+
+    for when in ["조식", "중식", "석식", "일품(중식)", "일품(석식)"]:
+        browser.get(url)
+        element = browser.find_element_by_name("menu")
+        browser.switch_to.frame(element)
+        select = Select(browser.find_element_by_name("food_div_cd"))
+        select.select_by_visible_text(when)
+
+        # 조회버튼 클릭
+        browser.find_element_by_xpath('/html/body/form/table/tbody/tr[3]/td/table/tbody/tr/td[8]/input').click()
 
         browser.switch_to.default_content()
         browser.switch_to.frame("bottom")
@@ -129,7 +138,6 @@ def get_recipe(cafeteria):
             answer += "금일 운영을 하지 않습니다."
         answer += "\n"
         count += 1
-    browser.quit()
     return answer
 
 
@@ -143,15 +151,8 @@ def make_answer_food_menu(user_answer=''):
     }
 
     cafeteria = cafeterias[user_answer]
-    return get_recipe(cafeteria)
-
-
-
-
-
-
-
-
+    answer = get_recipe(cafeteria)
+    return answer
 
 
 
