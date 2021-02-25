0727bf6248ba3bf2b2f5d43bb52039e2cbd34b83
ChanHyukPark-Tech <pgg6713@gmail.com>
2021-02-25 14:43:21 +0900

    양식까지 브레이크타임 정렬확인

--- chatbotapp/cnudata/food_court_time.py
+++ chatbotapp/cnudata/food_court_time.py
@@ -30,6 +30,66 @@ def ramen_time():
     else :
         response_text = "🍜라면코너 운영 시간 안내🍜\n\t토요일 11:00 ~ 14:30 \n\t 토요일은 사정에 따라 코너별 운영이 변동 될 수 있습니다."
         answer = insert_text(response_text)
+
+    reply = make_reply("⏰다른코너 운영 시간", "운영시간")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🍜라면코너 메뉴 보기", "라면&우동")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈푸드코트 메뉴 보기", "제1학생회관")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈다른식당 메뉴 보기", "학식")
+    answer = insert_replies(answer, reply)
+
+    return answer
+
+def gansik_time():
+
+    if not is_vacation:
+        response_text = "🍙간식코너 운영 시간 안내🍙\n☀️평일 중식 : 08:20 ~ 19:00\n🌙평일 석식 : 08:20 ~ 19:00"
+        answer = insert_text(response_text)
+    else :
+        response_text = "🍜간식코너 운영 시간 안내🍜\n\t토요일 11:00 ~ 14:30 \n\t 토요일은 사정에 따라 코너별 운영이 변동 될 수 있습니다."
+        answer = insert_text(response_text)
+
+    reply = make_reply("⏰다른코너 운영 시간", "운영시간")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🍜간식코너 메뉴 보기", "간식")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈푸드코트 메뉴 보기", "제1학생회관")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈다른식당 메뉴 보기", "학식")
+    answer = insert_replies(answer, reply)
+
+    return answer
+
+def america_time():
+
+    if not is_vacation:
+        response_text = "🍝양식코너 운영 시간 안내🍝\n☀️평일 중식 : 11:00 ~ 14:30\n🌙평일 석식 : 16:30 ~ 19:00\n⏳Break Time : 14:30 ~ 16:30"
+        answer = insert_text(response_text)
+    else :
+        response_text = "🍜양식코너 운영 시간 안내🍜\n\t토요일 11:00 ~ 14:30 \n\t 토요일은 사정에 따라 코너별 운영이 변동 될 수 있습니다."
+        answer = insert_text(response_text)
+        
+    reply = make_reply("⏰다른코너 운영 시간", "운영시간")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🍜양식코너 메뉴 보기", "양식")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈푸드코트 메뉴 보기", "제1학생회관")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈다른식당 메뉴 보기", "학식")
+    answer = insert_replies(answer, reply)
+
+    return answer
+
+def ramen_time():
+
+    if not is_vacation:
+        response_text = "🍜라면코너 운영 시간 안내🍜\n☀️평일 중식 : 08:20 ~ 19:00\n🌙평일 석식 : 08:20 ~ 19:00"
+        answer = insert_text(response_text)
+    else :
+        response_text = "🍜라면코너 운영 시간 안내🍜\n\t토요일 11:00 ~ 14:30 \n\t 토요일은 사정에 따라 코너별 운영이 변동 될 수 있습니다."
+        answer = insert_text(response_text)
     reply = make_reply("⏰다른코너 운영 시간", "운영시간")
     answer = insert_replies(answer, reply)
     reply = make_reply("🌈푸드코트 메뉴 보기", "제1학생회관")
@@ -38,5 +98,74 @@ def ramen_time():
     answer = insert_replies(answer, reply)
 
     return answer
+
+def ramen_time():
+
+    if not is_vacation:
+        response_text = "🍜라면코너 운영 시간 안내🍜\n☀️평일 중식 : 08:20 ~ 19:00\n🌙평일 석식 : 08:20 ~ 19:00"
+        answer = insert_text(response_text)
+    else :
+        response_text = "🍜라면코너 운영 시간 안내🍜\n\t토요일 11:00 ~ 14:30 \n\t 토요일은 사정에 따라 코너별 운영이 변동 될 수 있습니다."
+        answer = insert_text(response_text)
+    reply = make_reply("⏰다른코너 운영 시간", "운영시간")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈푸드코트 메뉴 보기", "제1학생회관")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈다른식당 메뉴 보기", "학식")
+    answer = insert_replies(answer, reply)
+
+    return answer
+
+def ramen_time():
+
+    if not is_vacation:
+        response_text = "🍜라면코너 운영 시간 안내🍜\n☀️평일 중식 : 08:20 ~ 19:00\n🌙평일 석식 : 08:20 ~ 19:00"
+        answer = insert_text(response_text)
+    else :
+        response_text = "🍜라면코너 운영 시간 안내🍜\n\t토요일 11:00 ~ 14:30 \n\t 토요일은 사정에 따라 코너별 운영이 변동 될 수 있습니다."
+        answer = insert_text(response_text)
+    reply = make_reply("⏰다른코너 운영 시간", "운영시간")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈푸드코트 메뉴 보기", "제1학생회관")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈다른식당 메뉴 보기", "학식")
+    answer = insert_replies(answer, reply)
+
+    return answer
+
+def ramen_time():
+
+    if not is_vacation:
+        response_text = "🍜라면코너 운영 시간 안내🍜\n☀️평일 중식 : 08:20 ~ 19:00\n🌙평일 석식 : 08:20 ~ 19:00"
+        answer = insert_text(response_text)
+    else :
+        response_text = "🍜라면코너 운영 시간 안내🍜\n\t토요일 11:00 ~ 14:30 \n\t 토요일은 사정에 따라 코너별 운영이 변동 될 수 있습니다."
+        answer = insert_text(response_text)
+    reply = make_reply("⏰다른코너 운영 시간", "운영시간")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈푸드코트 메뉴 보기", "제1학생회관")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈다른식당 메뉴 보기", "학식")
+    answer = insert_replies(answer, reply)
+
+    return answer
+
+def ramen_time():
+
+    if not is_vacation:
+        response_text = "🍜라면코너 운영 시간 안내🍜\n☀️평일 중식 : 08:20 ~ 19:00\n🌙평일 석식 : 08:20 ~ 19:00"
+        answer = insert_text(response_text)
+    else :
+        response_text = "🍜라면코너 운영 시간 안내🍜\n\t토요일 11:00 ~ 14:30 \n\t 토요일은 사정에 따라 코너별 운영이 변동 될 수 있습니다."
+        answer = insert_text(response_text)
+    reply = make_reply("⏰다른코너 운영 시간", "운영시간")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈푸드코트 메뉴 보기", "제1학생회관")
+    answer = insert_replies(answer, reply)
+    reply = make_reply("🌈다른식당 메뉴 보기", "학식")
+    answer = insert_replies(answer, reply)
+
+    return answer
+
 
 
