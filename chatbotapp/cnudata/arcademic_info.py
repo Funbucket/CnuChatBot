9fcfefce3976c0f20c33ab3fe16b9f96f2a9d58f
ChanHyukPark-Tech <pgg6713@gmail.com>
2021-02-27 02:49:33 +0900

    기타정보 수정  홈으로 지움

--- chatbotapp/cnudata/arcademic_info.py
+++ chatbotapp/cnudata/arcademic_info.py
@@ -53,7 +53,7 @@ def get_arcademic_answer():
                             {
                                 "title": get_subtitles()[0],
                                 "description": get_days()[0],
-                                "imageUrl": "https://plus.cnu.ac.kr//images/kr/sub01/ci_19_cnu1.png",
+                                "imageUrl": None,
                                 "link": {
                                     "web": get_urls()[0]
                                 }
@@ -61,7 +61,7 @@ def get_arcademic_answer():
                             {
                                 "title": get_subtitles()[1],
                                 "description": get_days()[1],
-                                "imageUrl": "https://plus.cnu.ac.kr//images/kr/sub01/ci_19_cnu1.png",
+                                "imageUrl": None,
                                 "link": {
                                     "web": get_urls()[1]
                                 }
@@ -69,7 +69,7 @@ def get_arcademic_answer():
                             {
                                 "title": get_subtitles()[2],
                                 "description": get_days()[2],
-                                "imageUrl": "https://plus.cnu.ac.kr//images/kr/sub01/ci_19_cnu1.png",
+                                "imageUrl": None,
                                 "link": {
                                     "web": get_urls()[2]
                                 }
@@ -77,7 +77,7 @@ def get_arcademic_answer():
                             {
                                 "title": get_subtitles()[3],
                                 "description": get_days()[3],
-                                "imageUrl": "https://plus.cnu.ac.kr//images/kr/sub01/ci_19_cnu1.png",
+                                "imageUrl": None,
                                 "link": {
                                     "web": get_urls()[3]
                                 }
@@ -85,7 +85,7 @@ def get_arcademic_answer():
                             {
                                 "title": get_subtitles()[4],
                                 "description": get_days()[4],
-                                "imageUrl": "https://plus.cnu.ac.kr//images/kr/sub01/ci_19_cnu1.png",
+                                "imageUrl": None,
                                 "link": {
                                     "web": get_urls()[4]
                                 }
--- chatbotapp/cnudata/etc.py
+++ chatbotapp/cnudata/etc.py
@@ -4,8 +4,8 @@ from chatbotapp.kakaojsonformat.response import *
 def get_entire_etc_answer():
     text = "🎸기타 페이지 입니다🎸"
     answer = insert_text(text)
-    reply = make_reply("🏡홈으로", "홈")
-    answer = insert_replies(answer, reply)
+    # reply = make_reply("🏡홈으로", "홈")
+    # answer = insert_replies(answer, reply)
     reply = make_reply("ℹ️개발자 정보", "ℹ️개발자 정보")
     answer = insert_replies(answer, reply)
     reply = make_reply("📬오류 제보 / 기능 건의📬", "📬오류 제보 / 기능 건의📬")
@@ -18,8 +18,8 @@ def get_error_answer():
 
     openurl = "https://open.kakao.com/o/sm0C6bZc"
     answer = insert_text("⁉️오류제보 / 기능 건의 ⁉️\n {}\n 링크를 클릭후 \n 편하게 채팅해주세요\n 여러분들의 오류제보가 \n 백마봇을 더 성장시킵니다".format(openurl))
-    reply = make_reply("🏡홈으로", "홈")
-    answer = insert_replies(answer, reply)
+    # reply = make_reply("🏡홈으로", "홈")
+    # answer = insert_replies(answer, reply)
     reply = make_reply("ℹ️개발자 정보", "ℹ️개발자 정보")
     answer = insert_replies(answer, reply)
     return answer
@@ -29,8 +29,8 @@ def get_information_answer():
 
     our_information = "😎 \t충남대백마봇 공동개발자 \t😎\n\n🥳  \t\t\t\t조해창\t\t\t\t\t🥳\n\t\t\touchc@icloud.com\t\n\n🤩  \t\t\t\t박찬혁\t\t\t\t\t🤩\n\tchanhyuk-tech@kakao.com\t\n"
     answer = insert_text(our_information)
-    reply = make_reply("🏡홈으로", "홈")
-    answer = insert_replies(answer, reply)
+    # reply = make_reply("🏡홈으로", "홈")
+    # answer = insert_replies(answer, reply)
     reply = make_reply("📬오류 제보 / 기능 건의📬", "📬오류 제보 / 기능 건의📬")
     answer = insert_replies(answer, reply)
 
