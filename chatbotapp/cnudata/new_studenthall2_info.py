0353900d519b8a3c5f3d785c5309fc02b00f8df8
ChanHyukPark-Tech <pgg6713@gmail.com>
2021-03-02 00:34:20 +0900

    레스토랑 안에 tr 을 인식하는 바람에 align 값지정

--- chatbotapp/cnudata/new_studenthall2_info.py
+++ chatbotapp/cnudata/new_studenthall2_info.py
@@ -8,15 +8,12 @@ req.raise_for_status()
 soup = BeautifulSoup(req.content.decode('utf8', 'replace'), 'html.parser')
 
 
-
-
 # 제2학생회관 , 3학생회관을 받아서 메뉴를 return 해주는 함수
 def get_studenthall23_answer_info(name):
     # tr의 2번쨰는 학생식당을 뜻한다 tr의 3번째는 교직원식당 tr의 4번째는 학생식당(백반외 일품요리)
-    student = soup.find("table", attrs={"class": "tab_color"}).find_all("tr")[2]
-    teacher = soup.find("table", attrs={"class": "tab_color"}).find_all("tr")[3]
-    student_special = soup.find("table", attrs={"class": "tab_color"}).find_all("tr")[4]
-
+    student = soup.find("table", attrs={"class": "tab_color"}).find_all("tr", attrs={"align": "center"})[2]
+    teacher = soup.find("table", attrs={"class": "tab_color"}).find_all("tr", attrs={"align": "center"})[3]
+    student_special = soup.find("table", attrs={"class": "tab_color"}).find_all("tr", attrs={"align": "center"})[4]
     if name == "제2학생회관":
         student_menu = student.find_all("td")[2].get_text().strip()
         teacher_menu = teacher.find_all("td")[1].get_text().strip()
@@ -36,5 +33,4 @@ def get_studenthall23_answer_info(name):
     answer = insert_replies(answer, reply)
 
     return answer
-
 
