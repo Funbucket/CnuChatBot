98f2d5569dda9f05a89459051dbcfb1ceeaa42d3
ChanHyukPark-Tech <pgg6713@gmail.com>
2021-03-03 01:11:34 +0900

    no message

--- chatbotapp/cnudata/bus_location2.py
+++ chatbotapp/cnudata/bus_location2.py
@@ -18,11 +18,10 @@ def get_make_time(hour, minute, second):
     return departure_time
 
 
-
     # 현재 가장 최근 출발한 놈과 , 그 놈이 얼마만큼 시간경과했는지 알려줌
     # who 랑 how 16:07 분에 찍는다 그럼 who = 16:00  how = 00:07
 def get_a1_who_how():
-    now = get_departure_time(8,46,26)
+    now = get_departure_time(8,35,00)
 
     start_time_1 = get_departure_time(8,30,0)
     start_time_2 = get_departure_time(9,00,0)
@@ -55,40 +54,82 @@ def get_a1_who_how():
     processed_start_time_13 = start_time_13 + timedelta(minutes=16,seconds=20)
     processed_start_time_14 = start_time_14 + timedelta(minutes=16,seconds=20)
     processed_start_time_15 = start_time_15 + timedelta(minutes=16,seconds=20)
-
+    # 각 if 앞에 걸리면 정상 출력 2번째 if 에 걸리면 미운행이므로 다음꺼 알려줌
     if start_time_1 <= now <= processed_start_time_1:
         return start_time_1 , (now - start_time_1).total_seconds()
     if processed_start_time_1 < now <= start_time_2:
         return start_time_2 , start_time_2 - now
+
     if start_time_2 < now <= processed_start_time_2:
         return start_time_2 , (now - start_time_2).total_seconds()
+    if processed_start_time_2 < now <= start_time_3:
+        return start_time_3 , start_time_3 - now
+
     if start_time_3 < now <= processed_start_time_3:
         return start_time_3 , (now - start_time_3).total_seconds()
+    if processed_start_time_3 < now <= start_time_4:
+        return start_time_4 , start_time_4 - now
+
     if start_time_4 < now <= processed_start_time_4:
         return start_time_4 , (now - start_time_4).total_seconds()
+    if processed_start_time_4 < now <= start_time_5:
+        return start_time_5 , start_time_5 - now
+
     if start_time_5 < now <= processed_start_time_5:
         return start_time_5 , (now - start_time_5).total_seconds()
+    if processed_start_time_5 < now <= start_time_6:
+        return start_time_6 , start_time_6 - now
+
     if start_time_6 < now <= processed_start_time_6:
         return start_time_6 , (now - start_time_6).total_seconds()
+    if processed_start_time_6 < now <= start_time_7:
+        return start_time_7 , start_time_7 - now
+
     if start_time_7 < now <= processed_start_time_7:
         return start_time_7 , (now - start_time_7).total_seconds()
+    if processed_start_time_7 < now <= start_time_8:
+        return start_time_8 , start_time_8 - now
+
     if start_time_8 < now <= processed_start_time_8:
         return start_time_8 , (now - start_time_8).total_seconds()
+    if processed_start_time_8 < now <= start_time_9:
+        return start_time_9 , start_time_9 - now
+
     if start_time_9 < now <= processed_start_time_9:
         return start_time_9 , (now - start_time_9).total_seconds()
+    if processed_start_time_9 < now <= start_time_10:
+        return start_time_10 , start_time_10 - now
+
     if start_time_10 < now <= processed_start_time_10:
         return start_time_10 , (now - start_time_10).total_seconds()
+    if processed_start_time_10 < now <= start_time_11:
+        return start_time_11 , start_time_11 - now
+
     if start_time_11 < now <= processed_start_time_11:
         return start_time_11 , (now - start_time_11).total_seconds()
+    if processed_start_time_11 < now <= start_time_12:
+        return start_time_12, start_time_12 - now
+
     if start_time_12 < now <= processed_start_time_12:
         return start_time_12 , (now - start_time_12).total_seconds()
+    if processed_start_time_12 < now <= start_time_13:
+        return start_time_13 , start_time_13 - now
+
     if start_time_13 < now <= processed_start_time_13:
         return start_time_13 , (now - start_time_13).total_seconds()
+    if processed_start_time_13 < now <= start_time_14:
+        return start_time_14 , start_time_14 - now
+
     if start_time_14 < now <= processed_start_time_14:
         return start_time_14 , (now - start_time_14).total_seconds()
+    if processed_start_time_14 < now <= start_time_15:
+        return start_time_15 , start_time_15 - now
+
     if start_time_15 < now <= processed_start_time_15:
         return start_time_15 , (now - start_time_15).total_seconds()
-
+    # 운행종료
+    if processed_start_time_15 < now or now.strftime("%H:%M:%S") < start_time_1.strftime("%H:%M:%S"):
+        return start_time_1, start_time_1
 # how 는 얼마만큼 차이가나는 8시30열차가 출발하면 8시38분에찍은애는 how는 8분이야
 # who 는 11시출발놈이다 11시30출발놈이다 넌 13:30출발놈이다
 # 8시30 < who  8분 < how 8시38분에 찍는다면
@@ -129,11 +170,17 @@ def get_a1_answer():
         elif how>=970:
             where_now = "산학연구교육연구원 앞"
 
-        answer = "A-1호차\n{}에 출발한 버스입니다\n현재 예상위치 \n {}".format(who,where_now)
+        who = who.strftime("%H:%M:%S")
+        answer = "A-1호차\n[출발지]:정심화국제문화회관\n{}에 출발한 버스입니다\n[현재 예상위치]\n{}".format(who,where_now)
         return answer
     #현재시간이 배차가 없는시간이라면 다음차 남은시간을 알려준다
+    elif who == how:
+        who = who.strftime("%H:%M:%S")
+        answer = "금일 A-1호차 운행종료 \n평일 첫차 {}".format(who)
+        return answer
     else:
-        answer = "A-1호차현재운행차없스빈다 다음차 {} 까지 {}남았습니다.".format(who,how)
+        who = who.strftime("%H:%M:%S")
+        answer = "A-1호차현재운행차 없습니다. \n다음차 {} 까지 \n{}남았습니다.".format(who,how)
         return answer
 
 print(get_a1_answer())
