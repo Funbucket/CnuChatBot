ed4d97361a94ab383733d539d17b76beedef106a
ChanHyukPark-Tech <pgg6713@gmail.com>
2021-03-06 01:00:07 +0900

    제 2학생회관 , 제3학생회관 주말에따른분류및 금액 추가

--- chatbotapp/cnudata/new_studenthall2_info.py
+++ chatbotapp/cnudata/new_studenthall2_info.py
@@ -1,45 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
 from chatbotapp.kakaojsonformat.response import *
-
-url = "http://cnuis.cnu.ac.kr/jsp/etc/toDayMenu.jsp"
-req = requests.get(url)
-req.raise_for_status()
-soup = BeautifulSoup(req.content.decode('utf8', 'replace'), 'html.parser')
-
-
+from chatbotapp.cnudata.is_vacation import get_vacation
 # 제2학생회관 , 3학생회관을 받아서 메뉴를 return 해주는 함수
 def get_studenthall23_answer_info(name):
-    # tr의 2번쨰는 학생식당을 뜻한다 tr의 3번째는 교직원식당 tr의 4번째는 학생식당(백반외 일품요리)
-    student = soup.find("table", attrs={"class": "tab_color"}).find_all("tr", attrs={"align": "center"})[2]
-    teacher = soup.find("table", attrs={"class": "tab_color"}).find_all("tr", attrs={"align": "center"})[3]
-    student_special = soup.find("table", attrs={"class": "tab_color"}).find_all("tr", attrs={"align": "center"})[4]
-    if name == "제2학생회관":
-        student_menu = student.find_all("td")[2].get_text().strip()
-        teacher_menu = teacher.find_all("td")[1].get_text().strip()
-        student_special_menu = student_special.find_all("table" ,attrs={"width":"100%"})[0].find("td").get_text().strip()
+    url = "http://localhost:63342/real_chatbot_project/weekmenu.html?_ijt=aubf9e16t1rgoisgr7457r6olt"
+    req = requests.get(url)
+    req.raise_for_status()
+    soup = BeautifulSoup(req.content.decode('utf8', 'replace'), 'html.parser')
 
-    elif name == "제3학생회관":
-        student_menu = student.find_all("td",attrs={"colspan":"2"})[1].get_text().strip()
-        teacher_menu = teacher.find_all("table" ,attrs={"width":"100%"})[1].get_text().strip()
-        student_special_menu = student_special.find_all("td")[4].get_text().strip()
-    student_menu = ' '.join(student_menu.split())
-    student_menu = student_menu.replace(" ", "\n")
-    teacher_menu = ' '.join(teacher_menu.split())
-    teacher_menu = teacher_menu.replace(" ", "\n")
-    student_special_menu = ' '.join(student_special_menu.split())
-    student_special_menu = student_special_menu.replace(" ", "\n")
+    #주말이라면
+    if get_vacation():
+        response_text = f"주말 및 공휴일에는\n {name}식당을 \n운영하지 않습니다\n평일에 방문해주세요😚\n"
+        answer = insert_text(response_text)
+        reply = make_reply("다른 식당 메뉴보기", "학식")
+        answer = insert_replies(answer, reply)
+        return answer
 
-    response_text = f"😚{name} 중식메뉴😚\n\n"
-    response_text += "👉학생식당\n" + student_menu + "\n"
-    response_text += "\n👉교직원식당\n" + teacher_menu + "\n"
-    response_text += "\n👉학생식당(일품)\n" + student_special_menu
+    else:
+        # tr의 2번쨰는 학생식당을 뜻한다 tr의 3번째는 교직원식당 tr의 4번째는 학생식당(백반외 일품요리)
+        student = soup.find("table", attrs={"class": "tab_color"}).find_all("tr", attrs={"align": "center"})[2]
+        teacher = soup.find("table", attrs={"class": "tab_color"}).find_all("tr", attrs={"align": "center"})[3]
+        student_special = soup.find("table", attrs={"class": "tab_color"}).find_all("tr", attrs={"align": "center"})[4]
+        if name == "제2학생회관":
+            student_menu = student.find_all("td", attrs={"height":"20"})[2].get_text().strip()
+            student_price = student.find_all("td", attrs={"height":"20"})[3].get_text().strip()
+            student_price = "[" + student_price + "]원\n"
+            teacher_menu = teacher.find_all("td", attrs={"height":"20"})[1].get_text().strip()
+            teacher_price = teacher.find_all("td", attrs={"height": "20"})[2].get_text().strip()
+            teacher_price = "[" + teacher_price + "]원\n"
+            student_special_menu = student_special.find_all("table", attrs={"width":"100%"})[0].find_all("td")[0].get_text().strip()
+            student_special_price = student_special.find_all("table", attrs={"width":"100%"})[0].find_all("td")[1].get_text().strip() + "원"
 
-    answer = insert_text(response_text)
-    reply = make_reply("다른 식당 메뉴보기", "학식")
-    answer = insert_replies(answer, reply)
+        elif name == "제3학생회관":
+            student_menu = student.find_all("td",attrs={"colspan":"2"})[1].get_text().strip()
+            student_price = ""
+            teacher_menu = teacher.find_all("table" ,attrs={"width":"100%"})[1].get_text().strip()
+            teacher_price = teacher.find_all("td", attrs={"height": "20"})[4].get_text().strip()
+            student_special_menu = student_special.find_all("td")[4].get_text().strip()
+            student_special_price = ""
 
-    return answer
+        student_menu = ' '.join(student_menu.split()).replace(" ", "\n")
+        teacher_menu = ' '.join(teacher_menu.split()).replace(" ", "\n")
+        student_special_menu = ' '.join(student_special_menu.split()).replace(" ", "\n")
+
+        response_text = f"😚{name} 중식메뉴😚\n\n"
+        response_text += "👉학생식당" + student_price + student_menu + "\n"
+        response_text += "\n👉교직원식당" + teacher_price + teacher_menu + "\n"
+        response_text += "\n👉학생식당(일품)" + student_special_price + student_special_menu
+        # print(response_text)
+        answer = insert_text(response_text)
+        reply = make_reply("다른 식당 메뉴보기", "학식")
+        answer = insert_replies(answer, reply)
+
+        return answer
 
 
 
