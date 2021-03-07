de8741d5aff69d5daa3a372c6a21cd35305630ac
ChanHyukPark-Tech <pgg6713@gmail.com>
2021-03-08 00:06:42 +0900

    제2학생회관 + 제 3학생회관 error modify

--- chatbotapp/cnudata/cafeteria/new_studenthall2_info.py
+++ chatbotapp/cnudata/cafeteria/new_studenthall2_info.py
@@ -36,19 +36,19 @@ def get_studenthall23_answer_info(name):
         if name == "제2학생회관":
             student_menu = student.find_all("td", attrs={"height":"20"})[2].get_text().strip()
             student_price = student.find_all("td", attrs={"height":"20"})[3].get_text().strip()
-            student_price = "[" + student_price + "]원\n"
+            student_price = "[" + student_price + "원]\n"
             teacher_menu = teacher.find_all("td", attrs={"height":"20"})[1].get_text().strip()
             teacher_price = teacher.find_all("td", attrs={"height": "20"})[2].get_text().strip()
-            teacher_price = "[" + teacher_price + "]원\n"
+            teacher_price = "[" + teacher_price + "원]\n"
             student_special_menu = student_special.find_all("table", attrs={"width":"100%"})[0].find_all("td")[0].get_text().strip()
-            student_special_price = "[" + student_special.find_all("table", attrs={"width":"100%"})[0].find_all("td")[1].get_text().strip() + "]원"
+            student_special_price = "[" + student_special.find_all("table", attrs={"width":"100%"})[0].find_all("td")[1].get_text().strip() + "원]"
 
         elif name == "제3학생회관":
             student_menu = "[" + student.find_all("td",attrs={"colspan":"2"})[1].get_text().strip() +"]"
             student_price = ""
             teacher_menu = teacher.find_all("table" ,attrs={"width":"100%"})[1].get_text().strip()
-            teacher_price = "[" + teacher.find_all("td", attrs={"height": "20"})[4].get_text().strip() + "]원"
-            student_special_menu = "[" + student_special.find_all("td")[4].get_text().strip() + "]"
+            teacher_price = "[" + teacher.find_all("td", attrs={"height": "20"})[4].get_text().strip() + "원]"
+            student_special_menu = "[" + student_special.find_all("td", attrs={"colspan": "2"})[1].get_text().strip() + "]"
             student_special_price = ""
 
         student_menu = ' '.join(student_menu.split()).replace(" ", "\n")
