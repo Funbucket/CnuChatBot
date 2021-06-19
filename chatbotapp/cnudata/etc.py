4499f7f03a07b2934b9783773a3335f3f2840d24
Joe <outofchng@gmail.com>
2021-06-19 16:11:57 +0900

    Refactor etc page

--- chatbotapp/cnudata/etc.py
+++ chatbotapp/cnudata/etc.py
@@ -27,7 +27,7 @@ def get_error_answer():
 
 def get_information_answer():
 
-    our_information = "\t\t\t\t조해창\t\t\t\t\t\n\t\touchc@icloud.com\t\n\n  \t\t\t\t박찬혁\t\t\t\t\t\nchanhyuk-tech@kakao.com\t\n"
+    our_information = "\n\n\t\t\t\t조해창\t\t\t\t\t\n\t\touchc@icloud.com\t\n\n  \t\t\t\t박찬혁\t\t\t\t\t\nchanhyuk-tech@kakao.com\t\n"
     answer = insert_text(our_information)
     # reply = make_reply("🏡홈으로", "홈")
     # answer = insert_replies(answer, reply)
