bccf80447d8b9671a62765982ed95bb6f56e0bb3
ChanhyukPark-Tech <69495129+ChanhyukPark-Tech@users.noreply.github.com>
2022-02-27 01:11:49 +0900

    Delete cnuchatbot-server directory

--- cnuchatbot-server/chatbotapp/cnudata/shuttlebus/time.py
+++
@@ -1,9 +0,0 @@
-from datetime import date, datetime, timedelta
-
-
-def get_time(hour, minute, second):
-    year = date.today().year
-    month = date.today().month
-    day = date.today().day
-    time = datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
-    return time
