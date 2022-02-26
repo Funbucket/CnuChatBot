58ba7a3e811694a33ad11ba7c781d67d62cb9900
Chanhyuk Park <pgg6713@gmail.com>
2022-02-27 02:33:00 +0900

    refactor: library refactor 완료

---
+++ cnuchatbot-server/chatbotapp/common/functions.py
@@ -0,0 +1,25 @@
+from pytimekr import pytimekr
+from datetime import date
+import datetime
+from chatbotapp.kakaojsonformat.response import *
+
+holiday_list = pytimekr.holidays()  # holidays메소드는 리스트 형태로 관련값 반환
+
+
+def is_holiday():
+    result = False
+    weekno = datetime.datetime.today().weekday()
+    for holiday in holiday_list:
+        if holiday == date.today() or weekno > 4:
+            result = True
+        else:
+            pass
+    return result
+
+
+def insert_multiple_reply(answer, replies):
+    for label, real in replies:
+        tempReply = make_reply(label, real)
+        answer = insert_replies(answer, tempReply)
+
+    return answer
---
+++ cnuchatbot-server/chatbotapp/common/variables/library.py
@@ -0,0 +1,8 @@
+# library
+
+library_BASE_URL = "https://clicker.cnu.ac.kr/Clicker/k/"
+libraryImage_BASE_URL = "https://library.cnu.ac.kr/image/ko/local/guide/floor"
+libraryKoreans = ["열람실", "📚 열람실"]
+
+floorImages = ["B2층 지도보기", "B1층 지도보기", "별관1층 지도보기",
+               "1층 지도보기", "2층 지도보기", "3층 지도보기", "4층 지도보기", "5층 지도보기"]
